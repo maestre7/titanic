2a054298d1710a9d587e86713c13cc16b73bf876
Alfredo Ruiz Santos <maestre7@gmail.com>
2023-05-18 18:13:45 +0200

    acabado

--- app.py
+++ app.py
@@ -66,9 +66,8 @@ X = train_df[variables]
 y = train_df['Survived']
 
 
-X = train_df[['Pclass', 'Age', 'Sex', 'SibSp', 'Parch', 'Fare', 'CabinBool', 'Embarked_C',
-'Embarked_S', 'Embarked_Q]]
-y = train_df[[Survived']]
+X = train_df[['Pclass', 'Age', 'Sex', 'SibSp', 'Parch', 'Fare', 'CabinBool', 'Embarked_C','Embarked_S', 'Embarked_Q']]
+y = train_df[['Survived']]
 X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=0)
 model = RandomForestClassifier(n_estimators=100, random_state=0)
 model.fit(X_train, y_train)
@@ -76,8 +75,50 @@ model.fit(X_train, y_train)
 model.fit(X_train, y_val)
 y_pred = model.predict(X_val)
 accuracy = accuracy_score(y_val, y_pred)
-importances =
-pd.DataFrame({'feature':X_train.columns,'importance':np.round(model.feature_importan
-ces_,3)})
-importances =
-importances.sort_values('importance',ascending=False).set_index('feature')
+importances =pd.DataFrame({'feature':X_train.columns,'importance':np.round(model.feature_importances_,3)})
+importances = importances.sort_values('importance',ascending=False).set_index('feature')
+
+'''
+Visualizamos la variable Clase
+'''
+survived_class = train_df[train_df['Survived']==1]['Pclass'].value_counts(sort=False)
+dead_class = train_df[train_df['Survived']==0]['Pclass'].value_counts(sort=False)
+class_df = pd.DataFrame([survived_class,dead_class],index=['Survived','Dead'])
+class_df.plot(kind='bar', stacked=True)
+plt.xlabel('Class')
+plt.ylabel('Number of passengers')
+plt.title('Survival rate by class')
+st.pyplot()
+
+'''
+Visualizamos la variable Sexo
+'''
+survived_sex = train_df[train_df['Survived']==1]['Sex'].value_counts(sort=False)
+dead_sex = train_df[train_df['Survived']==0]['Sex'].value_counts(sort=False)
+sex_df = pd.DataFrame([survived_sex,dead_sex],index=['Survived','Dead'])
+sex_df.plot(kind='bar', stacked=True)
+plt.xlabel('Sex')
+plt.ylabel('Number of passengers')
+plt.title('Survival rate by sex')
+st.pyplot()
+
+'''
+Visualizamos la variable Embarked
+'''
+survived_embark = train_df[train_df['Survived']==1]['Embarked'].value_counts(sort=False)
+dead_embark = train_df[train_df['Survived']==0]['Embarked'].value_counts(sort=False)
+embark_df =pd.DataFrame([survived_embark,dead_embark],index=['Survived','Dead'])
+embark_df.plot(kind='bar', stacked=True)
+plt.xlabel('Embarked')
+plt.ylabel('Number of passengers')
+plt.title('Survival rate by port of embarkation')
+st.pyplot()
+
+'''
+Visualizamos la importancia de las variables
+'''
+survived_embark = train_df[train_df['Survived']==1]['Embarked'].value_counts(sort=True)
+dead_embark = train_df[train_df['Survived']==0]['Embarked'].value_counts(sort=False)
+st.write('Visualización del modelo y datos')
+st.write('Importancia de características en el modelo')
+st.bar_chart(importances)
