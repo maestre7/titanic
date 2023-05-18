8667bd5628ab7fa70a6e53161e19c5d61d32244e
erika guzman <de969500@gmail.com>
2023-05-18 17:01:52 +0200

    part 1

--- app.py
+++ app.py
@@ -3,3 +3,14 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+ 
+# Developer 2 - Parte 1 
+ 
+''' 
+Hacemos Featuring Engineering 
+''' 
+train_df['Sex'] = train_df['Sex'].map({'male': 0, 'female': 1}) 
+test_df['Sex'] = test_df['Sex'].map({'male': 0, 'female': 1}) 
+train_df['Sex'].fillna(train_df['Age'].mean(), inplace=True) 
+test_df['Sex'].fillna(test_df['Age'].mean(), inplace=True)
