8a531b43b1565d174141e5bff84d25358c6a885d
erika guzman <de969500@gmail.com>
2023-05-18 17:37:53 +0200

    part 3

--- app.py
+++ app.py
@@ -47,3 +47,15 @@ train_df = pd.concat([train_df, pd.get_dummies(train_df['Embarked'],
 prefix='Embarked')], axis=0) 
 test_df = pd.concat([test_df, pd.get_dummies(test_df['Embarked'], prefix='Embarked')], 
 axis=0) 
+
+
+# Developer 2 - Parte 4 
+ 
+''' 
+Hacemos Featuring Engineering 
+''' 
+train_df = pd.concat([train_df, pd.get_dummies(train_df['Embarked'], 
+prefix='Embarked')], axis=1) 
+test_df = pd.concat([test_df, pd.get_dummies(test_df['Embarked'], prefix='Embarked')], 
+axis=1) 
+test_df['Fare'].fillna(test_df['Fare'].dropna().mean(), inplace=True)
